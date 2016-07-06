b58b355d8fa44548cc40dea63bbb60c4f8d91651
dryman <idryman@gmail.com>
2016-07-05 22:24:56 -0700

    update doc

--- search/pages_1.js
+++ search/pages_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['rationale_20and_20roadmap',['RATIONALE AND ROADMAP',['../md_doc_rationale.html',1,'']]]
+  ['rationale_20and_20roadmap',['RATIONALE AND ROADMAP',['../rationale.html',1,'']]]
 ];
