615f464385f9c0d46ca3903837a2235c998f89b5
dryman <idryman@gmail.com>
2016-10-09 12:18:04 -0700

    OPIC-30 first scratch on op_pspan

---
+++ src/opic/malloc/op_pspan.c
@@ -0,0 +1,196 @@
+/* op_pspan.c --- 
+ * 
+ * Filename: op_pspan.c
+ * Description: 
+ * Author: Felix Chern
+ * Maintainer: 
+ * Copyright: (c) 2016 Felix Chern
+ * Created: Sat Oct 8, 2016
+ * Version: 0.3.0
+ * Package-Requires: ()
+ * Last-Updated: 
+ *           By: 
+ *     Update #: 0
+ * URL: 
+ * Doc URL: 
+ * Keywords: 
+ * Compatibility: 
+ * 
+ */
+
+/* Commentary: 
+ * 
+ * 
+ * 
+ */
+
+/* Change Log:
+ * 
+ * 
+ */
+
+/* This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* Code: */
+
+#include "op_pspan.h"
+#include <stdatomic.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
+
+#define OP_PAGE_SIZE (1u << 12)
+
+void OPSingularPSpanInit(void* restrict addr, uint16_t ta_idx,
+                         uint16_t obj_size, uint8_t page_cnt)
+{
+  unsigned int obj_cnt, bitmap_cnt, padding, headroom;
+  obj_cnt = page_cnt * OP_PAGE_SIZE / obj_size;
+  bitmap_cnt = (obj_cnt + 64 - 1) >> 6;
+  padding = (bitmap_cnt << 6) - obj_cnt;
+  headroom = (sizeof(OPSingularPSpan) + bitmap_cnt*8 + obj_size - 1)/obj_size;
+  
+  OPSingularPSpan span = 
+    {
+      .ta_idx = ta_idx,
+      .obj_size = obj_size,
+      .page_cnt = page_cnt,
+      .bitmap_cnt = (uint8_t)bitmap_cnt,
+      .bitmap_headroom = (uint8_t)headroom,
+      .bitmap_padding = (uint8_t)padding
+    };
+
+  memcpy(addr, &span, sizeof(span));
+
+  addr += sizeof(OPSingularPSpan);
+
+  memset(addr, 0, bitmap_cnt << 3);
+  atomic_fetch_or((_Atomic uint64_t *) addr, (1UL << headroom)-1);
+  atomic_fetch_or((_Atomic uint64_t *) (addr + ((bitmap_cnt-1) << 3)),
+                  ~((1UL << (64-padding))-1));
+}
+
+void* OPSingularPSpanMalloc(OPSingularPSpan* self)
+{
+  _Atomic uint64_t *bitmap = (_Atomic uint64_t *)(self+1);
+  uint64_t old_bmap, new_bmap;
+  ptrdiff_t diff;
+  void* addr;
+  for (int i = 0; i < self->bitmap_cnt; i++)
+    {
+      do 
+        {
+          old_bmap = atomic_load_explicit(bitmap, memory_order_consume);
+          if (old_bmap == ~0UL) goto next_bmap;
+          new_bmap = (old_bmap + 1);
+          diff = __builtin_ctzl(new_bmap);
+          new_bmap |= old_bmap;
+        } while(!atomic_compare_exchange_weak(bitmap, &old_bmap, new_bmap));
+      
+      addr = (void*)self + ((i << 6) + diff) * self->obj_size;
+      *(uint16_t*)addr = self->ta_idx;
+      return addr;
+      
+    next_bmap:
+      bitmap++;
+      continue;
+    }
+  return NULL;
+}
+
+bool OPSingularPSpanFree(OPSingularPSpan* self, void* addr)
+{
+  ptrdiff_t diff = (addr - (void*)self) / self->obj_size;
+  // assert diff*obj_size + self == addr
+
+  _Atomic uint64_t *bitmap = (_Atomic uint64_t *)(self+1);
+  const int bmap_idx = diff >> 6;
+  const int remainder = diff - (bmap_idx << 6);
+  
+  atomic_fetch_and(&bitmap[bmap_idx], ~(1UL<<remainder));
+
+  if (self->bitmap_cnt == 1)
+    {
+      const uint64_t expected_empty_bmap =
+        ((1UL << self->bitmap_headroom)-1) |
+        ~((1UL << (64-self->bitmap_padding))-1);
+      if (atomic_compare_exchange_strong(bitmap,
+                                         &expected_empty_bmap,
+                                         ~0UL))
+      return true;
+    }
+  else if (bmap_idx == 0 &&
+           atomic_load(bitmap) == (1UL << self->bitmap_headroom)-1)
+    {
+      goto try_fullfree;
+    }
+  else if (bmap_idx == self->bitmap_cnt - 1 &&
+           atomic_load(&bitmap[bmap_idx]) ==
+           ~((1UL << (64-self->bitmap_padding))-1))
+    {
+      goto try_fullfree;
+    }
+  else if (!atomic_load(&bitmap[bmap_idx]))
+    {
+      goto try_fullfree;
+    }
+  else
+    return false;
+
+  int iter;
+  uint64_t expected_headroom, expected_body, expected_padding;
+  
+ try_fullfree:
+  
+  iter = 0;
+  expected_headroom = (1UL << self->bitmap_headroom)-1;
+  expected_body = ~0UL;
+  expected_padding = ~((1UL << (64-self->bitmap_padding))-1);
+
+  // headroom
+  if (!atomic_compare_exchange_strong(&bitmap[0],
+                                      &expected_headroom,
+                                      ~0UL))
+    return false;
+
+  // body
+  for (iter = 1; iter < self->bitmap_cnt - 1; iter++)
+    if (!atomic_compare_exchange_strong(&bitmap[iter],
+                                        &expected_body,
+                                        ~0UL))
+      goto catch_nonempty_exception;      
+
+  // padding
+  if (atomic_compare_exchange_strong(&bitmap[iter],
+                                     &expected_padding,
+                                     ~0UL))
+    return true;
+
+ catch_nonempty_exception:
+
+  // headroom
+  atomic_store(&bitmap[0], expected_headroom);
+
+  // body
+  for (int i = 1; i < iter; i++)
+    {
+      atomic_store(&bitmap[i], 0L);
+    }
+  
+  return false;
+}
+
+
+/* op_pspan.c ends here */
---
+++ src/opic/malloc/op_pspan.h
@@ -0,0 +1,76 @@
+/* op_pspan.h --- 
+ * 
+ * Filename: op_pspan.h
+ * Description: 
+ * Author: Felix Chern
+ * Maintainer: 
+ * Copyright: (c) 2016 Felix Chern
+ * Created: Sat Oct 8, 2016
+ * Version: 0.3.0
+ * Package-Requires: ()
+ * Last-Updated: 
+ *           By: 
+ *     Update #: 0
+ * URL: 
+ * Doc URL: 
+ * Keywords: 
+ * Compatibility: 
+ * 
+ */
+
+/* Commentary: 
+ * 
+ * 
+ * 
+ */
+
+/* Change Log:
+ * 
+ * 
+ */
+
+/* This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* Code: */
+
+#ifndef OP_PSPAN_H
+#define OP_PSPAN_H 1
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "../common/op_macros.h"
+#include "../common/op_assert.h"
+#include "../common/op_log.h"
+
+OP_BEGIN_DECLS
+
+typedef struct OPSingularPSpan OPSingularPSpan;
+
+struct OPSingularPSpan
+{
+  const uint16_t ta_idx;
+  const uint16_t obj_size;
+  const uint8_t page_cnt;
+  const uint8_t bitmap_cnt;
+  const uint8_t bitmap_headroom;
+  const uint8_t bitmap_padding;
+  OPSingularPSpan* prev;
+  OPSingularPSpan* next;
+};
+
+OP_END_DECLS
+
+#endif
+/* op_pspan.h ends here */
