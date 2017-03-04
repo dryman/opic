d329289091993ed502022a7ed563135ba2cbc055
dryman <idryman@gmail.com>
2017-03-03 19:33:45 -0800

    OPIC-33 op_atomic updates

--- src/opic/common/op_atomic.h
+++ src/opic/common/op_atomic.h
@@ -88,6 +88,14 @@ typedef _Atomic uint64_t a_uint64_t;
            a_int64_t*: atomic_book_critical_64) \
   (PUNCH_CARD)
 
+#define atomic_check_in_book(PUNCH_CARD)        \
+  _Generic((PUNCH_CARD),                        \
+           a_int8_t*:  atomic_check_in_book_8,  \
+           a_int16_t*: atomic_check_in_book_16, \
+           a_int32_t*: atomic_check_in_book_32, \
+           a_int64_t*: atomic_check_in_book_64) \
+  (PUNCH_CARD)
+
 #define atomic_enter_critical(PUNCH_CARD)               \
   _Generic((PUNCH_CARD),                                \
            a_int8_t*: atomic_enter_critical_8,          \
@@ -234,12 +242,80 @@ atomic_book_critical_64(a_int64_t* punch_card)
 }
 
 static inline void
+atomic_check_in_book_8(a_int8_t* punch_card)
+{
+  int8_t val = atomic_load_explicit(punch_card,
+                                    memory_order_relaxed);
+  do
+    {
+      if (val < 0)
+        return false;
+    }
+  while (!atomic_compare_exchange_weak_explicit
+         (punch_card, &val, (val + 1) | (1<<7),
+          memory_order_acquire,
+          memory_order_relaxed));
+  return true;
+}
+
+static inline void
+atomic_check_in_book_16(a_int16_t* punch_card)
+{
+  int16_t val = atomic_load_explicit(punch_card,
+                                    memory_order_relaxed);
+  do
+    {
+      if (val < 0)
+        return false;
+    }
+  while (!atomic_compare_exchange_weak_explicit
+         (punch_card, &val, (val + 1) | (1<<15),
+          memory_order_acquire,
+          memory_order_relaxed));
+  return true;
+}
+
+static inline void
+atomic_check_in_book_32(a_int32_t* punch_card)
+{
+  int32_t val = atomic_load_explicit(punch_card,
+                                    memory_order_relaxed);
+  do
+    {
+      if (val < 0)
+        return false;
+    }
+  while (!atomic_compare_exchange_weak_explicit
+         (punch_card, &val, (val + 1) | (1<<31),
+          memory_order_acquire,
+          memory_order_relaxed));
+  return true;
+}
+
+static inline void
+atomic_check_in_book_64(a_int64_t* punch_card)
+{
+  int64_t val = atomic_load_explicit(punch_card,
+                                    memory_order_relaxed);
+  do
+    {
+      if (val < 0)
+        return false;
+    }
+  while (!atomic_compare_exchange_weak_explicit
+         (punch_card, &val, (val + 1) | (1UL<<63),
+          memory_order_acquire,
+          memory_order_relaxed));
+  return true;
+}
+
+static inline void
 atomic_enter_critical_8(a_int8_t* punch_card)
 {
   while (atomic_load_explicit(punch_card, memory_order_relaxed)
          > INT8_MIN + 1)
     ;
-  atomic_sub_explicit(punch_card, 1, memory_order_acq_rel);
+  atomic_fetch_sub_explicit(punch_card, 1, memory_order_acq_rel);
 }
 
 static inline void
@@ -248,7 +324,7 @@ atomic_enter_critical_16(a_int16_t* punch_card)
   while (atomic_load_explicit(punch_card, memory_order_relaxed)
          > INT16_MIN + 1)
     ;
-  atomic_sub_explicit(punch_card, 1, memory_order_acq_rel);
+  atomic_fetch_sub_explicit(punch_card, 1, memory_order_acq_rel);
 }
 
 static inline void
@@ -257,7 +333,7 @@ atomic_enter_critical_32(a_int32_t* punch_card)
   while (atomic_load_explicit(punch_card, memory_order_relaxed)
          > INT32_MIN + 1)
     ;
-  atomic_sub_explicit(punch_card, 1, memory_order_acq_rel);
+  atomic_fetch_sub_explicit(punch_card, 1, memory_order_acq_rel);
 }
 
 static inline void
@@ -266,7 +342,7 @@ atomic_enter_critical_64(a_int64_t* punch_card)
   while (atomic_load_explicit(punch_card, memory_order_relaxed)
          > INT64_MIN + 1)
     ;
-  atomic_sub_explicit(punch_card, 1, memory_order_acq_rel);
+  atomic_fetch_sub_explicit(punch_card, 1, memory_order_acq_rel);
 }
 
 static inline void
@@ -293,6 +369,29 @@ atomic_exit_critical_64(a_int64_t* punch_card)
   atomic_store_explicit(punch_card, 1, memory_order_release);
 }
 
+static inline void
+atomic_exit_check_out_8(a_int8_t* punch_card)
+{
+  atomic_store_explicit(punch_card, 0, memory_order_release);
+}
+
+static inline void
+atomic_exit_check_out_16(a_int16_t* punch_card)
+{
+  atomic_store_explicit(punch_card, 0, memory_order_release);
+}
+
+static inline void
+atomic_exit_check_out_32(a_int32_t* punch_card)
+{
+  atomic_store_explicit(punch_card, 0, memory_order_release);
+}
+
+static inline void
+atomic_exit_check_out_64(a_int64_t* punch_card)
+{
+  atomic_store_explicit(punch_card, 0, memory_order_release);
+}
 
 OP_END_DECLS
 #endif
