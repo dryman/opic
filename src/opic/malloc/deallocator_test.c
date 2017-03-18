8d6feba0a4ce9ab329918983521edc5d19c87a2c
dryman <idryman@gmail.com>
2017-03-18 12:00:39 -0700

    OPIC-34 release 1 hpage test

--- src/opic/malloc/deallocator_test.c
+++ src/opic/malloc/deallocator_test.c
@@ -58,21 +58,125 @@
 #include "lookup_helper.h"
 #include "init_helper.h"
 #include "allocator.h"
+#include "deallocator.h"
+
+static void
+test_OPHeapReleaseHSpan_1Page(void** context)
+{
+  OPHeap* heap;
+  uintptr_t heap_base;
+  uint64_t occupy_bmap[HPAGE_BMAP_NUM] = {};
+  uint64_t header_bmap[HPAGE_BMAP_NUM] = {};
+  HugeSpanPtr hspan[8];
+  OPHeapCtx ctx;
+  Magic raw_hpage_magic = {}, typed_hpage_magic = {}, hblob_magic = {};
+
+  assert_true(OPHeapNew(&heap));
+  heap_base = (uintptr_t)heap;
+
+  occupy_bmap[0] = 0x0F;
+  occupy_bmap[1] = 0x0F;
+  header_bmap[0] = 0x0F;
+  header_bmap[1] = 0x0F;
+  heap->occupy_bmap[0] = 0x0F;
+  heap->header_bmap[0] = 0x0F;
+  heap->occupy_bmap[1] = 0x0F;
+  heap->header_bmap[1] = 0x0F;
+
+  raw_hpage_magic.raw_hpage.pattern = RAW_HPAGE_PATTERN;
+  typed_hpage_magic.typed_hpage.pattern = TYPED_HPAGE_PATTERN;
+  hblob_magic.huge_blob.pattern = HUGE_BLOB_PATTERN;
+  hblob_magic.huge_blob.huge_pages = 1;
+
+  hspan[0].hpage = &heap->hpage;
+  hspan[1].uintptr = heap_base + HPAGE_SIZE;
+  hspan[2].uintptr = heap_base + 2 * HPAGE_SIZE;
+  hspan[3].uintptr = heap_base + 3 * HPAGE_SIZE;
+  hspan[4].uintptr = heap_base + 64 * HPAGE_SIZE;
+  hspan[5].uintptr = heap_base + 65 * HPAGE_SIZE;
+  hspan[6].uintptr = heap_base + 66 * HPAGE_SIZE;
+  hspan[7].uintptr = heap_base + 67 * HPAGE_SIZE;
+  ctx.hspan = hspan[0];
+  HPageInit(&ctx, raw_hpage_magic);
+  ctx.hspan = hspan[1];
+  HPageInit(&ctx, raw_hpage_magic);
+  ctx.hspan = hspan[2];
+  HPageInit(&ctx, typed_hpage_magic);
+  ctx.hspan = hspan[3];
+  HPageInit(&ctx, typed_hpage_magic);
+  *hspan[4].magic = hblob_magic;
+  *hspan[5].magic = hblob_magic;
+  *hspan[6].magic = hblob_magic;
+  *hspan[7].magic = hblob_magic;
+
+  occupy_bmap[0] = 0x0E;
+  header_bmap[0] = 0x0E;
+  OPHeapReleaseHSpan(hspan[0]);
+  assert_memory_equal(occupy_bmap, heap->occupy_bmap, sizeof(occupy_bmap));
+  assert_memory_equal(header_bmap, heap->header_bmap, sizeof(header_bmap));
+  assert_int_equal(0, heap->pcard);
+
+  occupy_bmap[1] = 0x0E;
+  header_bmap[1] = 0x0E;
+  OPHeapReleaseHSpan(hspan[4]);
+  assert_memory_equal(occupy_bmap, heap->occupy_bmap, sizeof(occupy_bmap));
+  assert_memory_equal(header_bmap, heap->header_bmap, sizeof(header_bmap));
+  assert_int_equal(0, heap->pcard);
+
+  occupy_bmap[0] = 0x06;
+  header_bmap[0] = 0x06;
+  OPHeapReleaseHSpan(hspan[3]);
+  assert_memory_equal(occupy_bmap, heap->occupy_bmap, sizeof(occupy_bmap));
+  assert_memory_equal(header_bmap, heap->header_bmap, sizeof(header_bmap));
+  assert_int_equal(0, heap->pcard);
+
+  occupy_bmap[0] = 0x04;
+  header_bmap[0] = 0x04;
+  OPHeapReleaseHSpan(hspan[1]);
+  assert_memory_equal(occupy_bmap, heap->occupy_bmap, sizeof(occupy_bmap));
+  assert_memory_equal(header_bmap, heap->header_bmap, sizeof(header_bmap));
+  assert_int_equal(0, heap->pcard);
+
+  occupy_bmap[0] = 0x00;
+  header_bmap[0] = 0x00;
+  OPHeapReleaseHSpan(hspan[2]);
+  assert_memory_equal(occupy_bmap, heap->occupy_bmap, sizeof(occupy_bmap));
+  assert_memory_equal(header_bmap, heap->header_bmap, sizeof(header_bmap));
+  assert_int_equal(0, heap->pcard);
+
+  occupy_bmap[1] = 0x0C;
+  header_bmap[1] = 0x0C;
+  OPHeapReleaseHSpan(hspan[5]);
+  assert_memory_equal(occupy_bmap, heap->occupy_bmap, sizeof(occupy_bmap));
+  assert_memory_equal(header_bmap, heap->header_bmap, sizeof(header_bmap));
+  assert_int_equal(0, heap->pcard);
+
+  occupy_bmap[1] = 0x08;
+  header_bmap[1] = 0x08;
+  OPHeapReleaseHSpan(hspan[6]);
+  assert_memory_equal(occupy_bmap, heap->occupy_bmap, sizeof(occupy_bmap));
+  assert_memory_equal(header_bmap, heap->header_bmap, sizeof(header_bmap));
+  assert_int_equal(0, heap->pcard);
+
+  occupy_bmap[1] = 0x00;
+  header_bmap[1] = 0x00;
+  OPHeapReleaseHSpan(hspan[7]);
+  assert_memory_equal(occupy_bmap, heap->occupy_bmap, sizeof(occupy_bmap));
+  assert_memory_equal(header_bmap, heap->header_bmap, sizeof(header_bmap));
+  assert_int_equal(0, heap->pcard);
+
+  OPHeapDestroy(heap);
+}
 
 int
 main (void)
 {
-  /*
-  const struct CMUnitTest allocator_tests[] =
+  const struct CMUnitTest deallocator_tests[] =
     {
-      cmocka_unit_test(test_OPHeapObtainHPage_FullSize),
-      cmocka_unit_test(test_OPHeapObtainHPage_SmallSize),
-      cmocka_unit_test(test_OPHeapObtainHBlob_Small),
-      cmocka_unit_test(test_OPHeapObtainHBlob_Large),
+      cmocka_unit_test(test_OPHeapReleaseHSpan_1Page),
     };
 
-  return cmocka_run_group_tests(allocator_tests, NULL, NULL);
-  */
+  return cmocka_run_group_tests(deallocator_tests, NULL, NULL);
 }
 
 /* deallocator_test.c ends here */
