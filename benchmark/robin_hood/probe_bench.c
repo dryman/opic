6a2990f93fb0b6f2250614bc73f242d3640c5f00
dryman <idryman@gmail.com>
2017-06-26 22:32:48 -0700

    OPIC-54 probe benchmark

---
+++ benchmark/robin_hood/probe_bench.c
@@ -0,0 +1,436 @@
+/* probe_bench.c ---
+ *
+ * Filename: probe_bench.c
+ * Description:
+ * Author: Felix Chern
+ * Maintainer:
+ * Copyright: (c) 2017 Felix Chern
+ * Created: Mon Jun 26 21:55:19 2017 (-0700)
+ * Version:
+ * Package-Requires: ()
+ * Last-Updated:
+ *           By:
+ *     Update #: 0
+ * URL:
+ * Doc URL:
+ * Keywords:
+ * Compatibility:
+ *
+ */
+
+/* Commentary:
+ *
+ *
+ *
+ */
+
+/* Change Log:
+ *
+ *
+ */
+
+/* This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* Code: */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <stdbool.h>
+#include <inttypes.h>
+#include <unistd.h>
+#include "opic/common/op_assert.h"
+#include "opic/op_malloc.h"
+#include "opic/hash/op_hash.h"
+#include "generic_table.h"
+
+#include "murmurhash3.h"
+#include "spookyhash-c/spookyhash.h"
+#include "farmhash-c/farmhash.h"
+
+static int objcnt = 0;
+static uint64_t val_sum = 0;
+
+typedef uint64_t (*HashFunc)(void* key, void* context, OPHash hasher);
+typedef void (*RunKey)(int size, HashFunc hash_func,
+                       void* context, OPHash hasher);
+
+static void run_short_keys(int size, HashFunc hash_func,
+                           void* context, OPHash hasher);
+static void run_mid_keys(int size, HashFunc hash_func,
+                         void* context, OPHash hasher);
+static void run_long_keys(int size, HashFunc hash_func,
+                          void* context, OPHash hasher);
+static void run_long_int(int size, HashFunc hash_func,
+                         void* context, OPHash hasher);
+static void print_timediff(const char* info,
+                           struct timeval start, struct timeval end);
+
+uint64_t murmur3(void* key, size_t size)
+{
+  uint64_t hashed_val[2];
+  MurmurHash3_x64_128(key, size, 421439783, hashed_val);
+  return hashed_val[0];
+}
+
+uint64_t spooky(void* key, size_t size)
+{
+  return spookyhash64(key, size, 421439783);
+}
+
+uint64_t city(void* key, size_t size)
+{
+  return cityhash64(key, size);
+}
+
+uint64_t farm(void* key, size_t size)
+{
+  return farmhash64(key, size);
+}
+
+uint64_t LPInsertWrap(void* key, void* context, OPHash hash_impl)
+{
+  static uint64_t val = 0;
+  LPInsertCustom(context, hash_impl, key, &val);
+  val++;
+  return 0;
+}
+
+uint64_t QPInsertWrap(void* key, void* context, OPHash hash_impl)
+{
+  static uint64_t val = 0;
+  QPInsertCustom(context, hash_impl, key, &val);
+  val++;
+  return 0;
+}
+
+uint64_t DHInsertWrap(void* key, void* context, OPHash hash_impl)
+{
+  static uint64_t val = 0;
+  DHInsertCustom(context, hash_impl, key, &val);
+  val++;
+  return 0;
+}
+
+uint64_t LPGetWrap(void* key, void* context, OPHash hash_impl)
+{
+  return *(uint64_t*)LPGetCustom(context, hash_impl, key);
+}
+
+uint64_t QPGetWrap(void* key, void* context, OPHash hash_impl)
+{
+  return *(uint64_t*)QPGetCustom(context, hash_impl, key);
+}
+
+uint64_t DHGetWrap(void* key, void* context, OPHash hash_impl)
+{
+  return *(uint64_t*)DHGetCustom(context, hash_impl, key);
+}
+
+void help(char* program)
+{
+  printf
+    ("usage: %s [-n power_of_2] [-r repeat] [-k keytype] [-i impl]\n"
+     "       [-l load] [-p]\n"
+     "Options:\n"
+     "  -n num     Number of elements measured in power of 2.\n"
+     "             -n 20 => run 2^20 = 1 million elements.\n"
+     "             defaults to 20\n"
+     "  -r repeat  Repeat the benchmar for `repeat` times.\n"
+     "  -k keytype keytype = s_string, m_string, l_string l_int\n"
+     "             s_string: 6 bytes, m_string: 32 bytes,\n"
+     "             l_string: 256 bytes, l_int: 8 bytes\n"
+     "             For now only robin_hood hash supports long_int benchmark\n"
+     "  -i impl    impl = linear, quadratic, double_hashing\n"
+     "  -l load    load number range from 0.0 to 1.0.\n"
+     "  -p         print probing stats of hash table\n"
+     "  -h         print help.\n"
+     ,program);
+  exit(1);
+}
+
+int main(int argc, char* argv[])
+{
+  OPHeap* heap;
+  GenericTable* table;
+  struct timeval
+    i_start, i_end,
+    q_start, q_end;
+
+  int num_power, opt;
+  int repeat = 1;
+  RunKey key_func = run_short_keys;
+  int k_len = 6;
+  uint64_t num;
+  double load = 0.8;
+  bool print_stat = false;
+
+  HashFunc put_func = LPInsertWrap;
+  HashFunc get_func = LPGetWrap;
+  OPHash hasher = city;
+
+  num_power = 20;
+
+  while ((opt = getopt(argc, argv, "n:r:k:i:l:f:ph")) > -1)
+    {
+      switch (opt)
+        {
+        case 'n':
+          num_power = atoi(optarg);
+          break;
+        case 'r':
+          repeat = atoi(optarg);
+          break;
+        case 'k':
+          if (!strcmp("s_string", optarg))
+            {
+              key_func = run_short_keys;
+              k_len = 6;
+            }
+          else if (!strcmp("m_string", optarg))
+            {
+              key_func = run_mid_keys;
+              k_len = 32;
+            }
+          else if (!strcmp("l_string", optarg))
+            {
+              key_func = run_long_keys;
+              k_len = 256;
+            }
+          else if (!strcmp("l_int", optarg))
+            {
+              key_func = run_long_int;
+              k_len = 8;
+            }
+          else
+            help(argv[0]);
+          break;
+        case 'i':
+          if (!strcmp("linear", optarg))
+            {
+              printf("linear probing\n");
+            }
+          else if (!strcmp("quadratic", optarg))
+            {
+              put_func = QPInsertWrap;
+              get_func = QPGetWrap;
+              printf("quadratic probing\n");
+            }
+          else if (!strcmp("double_hashing", optarg))
+            {
+              put_func = DHInsertWrap;
+              get_func = DHGetWrap;
+              printf("double hashing\n");
+            }
+          else
+            help(argv[0]);
+          break;
+        case 'l':
+          load = atof(optarg);
+          break;
+        case 'f':
+          if (!strcmp("murmur3", optarg))
+            {
+              printf("using murmur3 hasher\n");
+              hasher = murmur3;
+            }
+          else if (!strcmp("spooky", optarg))
+            {
+              printf("using spookyhash\n");
+              hasher = spooky;
+            }
+          else if (!strcmp("city", optarg))
+            {
+              printf("using cityhash\n");
+              hasher = city;
+            }
+          else if (!strcmp("farm", optarg))
+            {
+              printf("using farmhash\n");
+              hasher = farm;
+            }
+          else
+            help(argv[0]);
+          break;
+        case 'p':
+          print_stat = true;
+          break;
+        case 'h':
+        case '?':
+        default:
+            help(argv[0]);
+        }
+    }
+
+  num = 1UL << num_power;
+  printf("running elements %" PRIu64 "\n", num);
+
+  op_assert(OPHeapNew(&heap), "Create OPHeap\n");
+
+  for (int i = 0; i < repeat; i++)
+    {
+      printf("attempt %d\n", i + 1);
+      op_assert(TableNew(heap, &table, num, load, k_len, 8), "Create Table\n");
+
+      gettimeofday(&i_start, NULL);
+      key_func(num_power, put_func, table, hasher);
+      gettimeofday(&i_end, NULL);
+
+      printf("insert finished\n");
+
+      gettimeofday(&q_start, NULL);
+      key_func(num_power, get_func, table, hasher);
+      gettimeofday(&q_end, NULL);
+
+      print_timediff("Funnel Insert time: ", i_start, i_end);
+      print_timediff("Funnel Query time: ", q_start, q_end);
+
+      if (print_stat)
+        {
+          TablePrintStat(table);
+        }
+      TableDestroy(table);
+    }
+
+  return 0;
+}
+
+void run_short_keys(int size, HashFunc hash_func,
+                    void* context, OPHash hasher)
+{
+  op_assert(size >= 12, "iteration size must > 2^12\n");
+  int i_bound = 1 << (size - 12);
+  char uuid [] = "!!!!!!";
+  uint64_t counter = 0;
+  for (int i = 0; i < i_bound; i++)
+    {
+      for (int j = 2, val = counter >> 12; j < 6; j++, val>>=6)
+        {
+          uuid[j] = 0x21 + (val & 0x3F);
+        }
+      for (int j = 0; j < 64; j++)
+        {
+          uuid[1] = 0x21 + j;
+          for (int k = 0; k < 64; k++)
+            {
+              uuid[0] = 0x21 + k;
+              counter++;
+              hash_func(uuid, context, hasher);
+            }
+        }
+    }
+}
+
+void run_mid_keys(int size, HashFunc hash_func,
+                  void* context, OPHash hasher)
+{
+  op_assert(size >= 12, "iteration size must > 2^12\n");
+  int i_bound = 1 << (size - 12);
+  char uuid [] = "!!!!!!--!!!!!!--!!!!!!--!!!!!!--";
+  uint64_t counter = 0;
+  for (int i = 0; i < i_bound; i++)
+    {
+      for (int j = 2, val = counter >> 12; j < 6; j++, val>>=6)
+        {
+          uuid[j] = 0x21 + (val & 0x3F);
+          uuid[j+8] = 0x21 + (val & 0x3F);
+          uuid[j+16] = 0x21 + (val & 0x3F);
+          uuid[j+24] = 0x21 + (val & 0x3F);
+        }
+      for (int j = 0; j < 64; j++)
+        {
+          uuid[1] = 0x21 + j;
+          uuid[1+8] = 0x21 + j;
+          uuid[1+16] = 0x21 + j;
+          uuid[1+24] = 0x21 + j;
+          for (int k = 0; k < 64; k++)
+            {
+              uuid[0] = 0x21 + k;
+              uuid[0+8] = 0x21 + k;
+              uuid[0+16] = 0x21 + k;
+              uuid[0+24] = 0x21 + k;
+              counter++;
+              hash_func(uuid, context, hasher);
+            }
+        }
+    }
+}
+
+void run_long_keys(int size, HashFunc hash_func,
+                   void* context, OPHash hasher)
+{
+  op_assert(size >= 12, "iteration size must > 2^12\n");
+  int i_bound = 1 << (size - 12);
+  char uuid [] =
+    "!!!!!!--!!!!!!--!!!!!!--!!!!!!--"
+    "!!!!!!--!!!!!!--!!!!!!--!!!!!!--"
+    "!!!!!!--!!!!!!--!!!!!!--!!!!!!--"
+    "!!!!!!--!!!!!!--!!!!!!--!!!!!!--"
+    "!!!!!!--!!!!!!--!!!!!!--!!!!!!--"
+    "!!!!!!--!!!!!!--!!!!!!--!!!!!!--"
+    "!!!!!!--!!!!!!--!!!!!!--!!!!!!--"
+    "!!!!!!--!!!!!!--!!!!!!--!!!!!!--";
+  uint64_t counter = 0;
+  for (int i = 0; i < i_bound; i++)
+    {
+      for (int j = 2, val = counter >> 12; j < 6; j++, val>>=6)
+        {
+          for (int k = 0; k < 256; k+=8)
+            uuid[j+k] = 0x21 + (val & 0x3F);
+        }
+      for (int j = 0; j < 64; j++)
+        {
+          for (int h = 0; h < 256; h+=8)
+            uuid[h+1] = 0x21 + j;
+          for (int k = 0; k < 64; k++)
+            {
+              for (int h = 0; h < 256; h+=8)
+                uuid[h] = 0x21 + k;
+              counter++;
+              hash_func(uuid, context, hasher);
+            }
+        }
+    }
+}
+
+void run_long_int(int size, HashFunc hash_func,
+                  void* context, OPHash hasher)
+{
+  op_assert(size >= 12, "iteration size must > 2^12\n");
+  uint64_t i_bound = 1 << size;
+  for (uint64_t i = 0; i < i_bound; i++)
+    {
+      hash_func(&i, context, hasher);
+    }
+}
+
+void print_timediff(const char* info, struct timeval start, struct timeval end)
+{
+  long int second = end.tv_sec - start.tv_sec;
+  unsigned int usec;
+  if (end.tv_usec < start.tv_usec)
+    {
+      second--;
+      usec = end.tv_usec + 1000000 - start.tv_usec;
+    }
+  else
+    usec = end.tv_usec - start.tv_usec;
+
+  printf("%s%ld.%06u\n", info, second, usec);
+}
+
+/* probe_bench.c ends here */
